7b4d254a82d299600ac8df4f4cddc5decb4ce330
csemlies <csemlies@uchicago.edu>
2026-02-15 20:28:53 +0000

    working on devise for sign in / out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,14 @@ class ApplicationController < ActionController::Base
   before_action :set_current_user
   helper_method :current_user
 
-  def current_user
-    @current_user
-  end
-
   private
 
   def set_current_user
-    @current_user = User.find_by(id: session[:user_id])
+    # prefer session (signed-in user), but allow user_id via params when you redirect links
+    @current_user = User.find_by(id: session[:user_id]) || User.find_by(id: params[:user_id])
+  end
+
+  def current_user
+    @current_user
   end
 end
--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -3,27 +3,29 @@ class InvitesController < ApplicationController
     @token = params[:token]
     @invite_link = InviteLink.find_by(token: @token)
     @plan_session = @invite_link&.plan_session
+
+    @default_name = "Name"
+    @default_phone = "000-000-0000"
   end
 
   def create
     token = params[:token]
-    phone = params[:phone_number].to_s.gsub(/\D/, "") # keep digits only
-    name  = params[:name].to_s.strip
+    phone = params[:phone_number].to_s.gsub(/\D/, "")
+    name = params[:name].to_s.strip
 
     invite_link = InviteLink.find_by!(token: token)
     plan_session = invite_link.plan_session
 
-    # Create/find user by phone & name
     user = User.find_or_initialize_by(phone_number: phone)
     user.name = name if user.name.blank?
     user.save!
 
-    # Add as member (avoid duplicates)
     SessionMember.find_or_create_by!(plan_session: plan_session, user: user)
 
+    # persist current user in session so nav and member label work
     session[:user_id] = user.id
 
     redirect_to new_plan_session_busy_block_path(plan_session, user_id: user.id),
-            notice: "You're in! Add your busy times."
+                notice: "You're in! Add your busy times."
   end
 end
