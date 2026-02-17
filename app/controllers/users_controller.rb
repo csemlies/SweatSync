514a1ca3926537151efa00c725c8452bc9f69db9
csemlies <csemlies@uchicago.edu>
2026-02-17 01:17:42 +0000

    working on profile errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,32 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, only: [:show]
+  before_action :set_user, only: [:show]
 
   def show
-    @user = params[:id].present? ? User.find(params[:id]) : current_user
+    owned = @user.plan_sessions
 
-    @owned_sessions = @user.plan_sessions.order(start_time: :desc)
+    participating = PlanSession
+                      .joins(:session_members)
+                      .where(session_members: { user_id: @user.id })
+                      .distinct
+
+    all_sessions = (owned.to_a + participating.to_a).uniq
+
+    now = Time.current
+
+    @active_sessions  = all_sessions.select { |s| s.start_time.present? && s.start_time >= now }
+                                    .sort_by(&:start_time)
+    @expired_sessions = all_sessions.select { |s| s.start_time.blank? || s.start_time < now }
+                                    .sort_by(&:start_time).reverse
+
+    @session_roles = {}
+    all_sessions.each do |s|
+      @session_roles[s.id] = (s.user_id == @user.id ? :host : :member) rescue :member
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
